71e7c9e0ab132319d774f67d383fcc8e019e55cf
SayramovAzizbek <sayramovazizbek@gmail.com>
2022-10-26 18:47:59 +0500

    Add modal about game, change some variables, required to selects

--- js/main.js
+++ js/main.js
@@ -1,45 +1,102 @@
-let siteBody = document.querySelector(".site-body");
+const siteBody = document.querySelector(".site-body");
+const enterGameSection = document.querySelector("#enter-game");
+const enterGameForm = document.querySelector(".enter-game-form");
+const chooseLevelSelect = document.querySelector(".choose_level-select");
+const chooseTimeSelect = document.querySelector(".choose_time-select");
+const enterGameBtn = document.querySelector(".enter-game-btn");
+const aboutGameBtn = document.querySelector(".about-game-btn");
+const aboutGameBox = document.querySelector(".game-introduce-box");
+const aboutCloseBtn = document.querySelector(".about-game-close-btn");
 
-let enterGameSection = document.querySelector("#enter-game");
-let enterGameForm = document.querySelector(".enter-game-form");
-
-let chooseLevelSelect = document.querySelector(".choose_level-select");
-let chooseTimeSelect = document.querySelector(".choose_time-select");
-let enterGameBtn = document.querySelector(".enter-game-btn");
-
-let mainGameSection = document.querySelector("#main-game-box");
-
-let gameScoreCountText = document.querySelector(".game-score-count");
-let gameUserChosenLevelText = document.querySelector(
+const mainGameSection = document.querySelector("#main-game-box");
+const gameScoreCountText = document.querySelector(".game-score-count");
+const gameUserChosenLevelText = document.querySelector(
   ".game-user-chosen-level-text"
 );
-let gameTimeText = document.querySelector(".game-time-text");
+const gameTimeText = document.querySelector(".game-time-text");
+const gameQuestionText = document.querySelector(".game-question-text");
+const gameCardList = document.querySelector(".game-card-list");
+const gameCardItemBox = document.querySelector(".game-card-box");
 
-let gameQuestionText = document.querySelector(".game-question-text");
-let gameCardList = document.querySelector(".game-card-list");
-let gameCardItemBox = document.querySelector(".game-card-box");
-
-let gameOverModal = document.querySelector(".game-over-modal");
-let gameErrorCounterText = document.querySelector(".game-modal-error-counter");
-let gameOverModalText = document.querySelector(".game-modal-text-game-over");
-let gameOverModalScoreCounter = document.querySelector(
+const gameOverModal = document.querySelector(".game-over-modal");
+const gameErrorCounterText = document.querySelector(
+  ".game-modal-error-counter"
+);
+const gameOverModalText = document.querySelector(".game-modal-text-game-over");
+const gameOverModalScoreCounter = document.querySelector(
   ".game-modal-score-counter"
 );
 
-let gameWinModal = document.querySelector(".game-win-modal");
-let gameWinModalScoreText = document.querySelector(".game-win-score-counter");
-let gameWinModalErrorText = document.querySelector(".game-win-error-counter");
-// let gameOverModalBtn = document.querySelector(".game-over-modal-btn");
+const gameWinModal = document.querySelector(".game-win-modal");
+const gameWinModalScoreText = document.querySelector(".game-win-score-counter");
+const gameWinModalErrorText = document.querySelector(".game-win-error-counter");
 
-let cardRulesTemplate = document.querySelector(".game-card-template").content;
+const cardRulesTemplate = document.querySelector(".game-card-template").content;
 
 let sortedData = [];
-
 let levelData = [];
 
 let gameScoreCount = 0;
 let gameErrorCounter = 0;
 gameScoreCountText.textContent = `Score: ${gameScoreCount}`;
+
+// ! Form before enter game
+enterGameForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  enterGameSection.classList.add("d-none");
+  mainGameSection.classList.add("d-block");
+
+  if (chooseLevelSelect.value === "easy") {
+    gameUserChosenLevelText.textContent = `You chose ${chooseLevelSelect.value} level`;
+    levelData = selectedEasyLevel(roadSymbolGenerated);
+    cardRender(levelData);
+  } else if (chooseLevelSelect.value === "medium") {
+    gameUserChosenLevelText.textContent = `You chose ${chooseLevelSelect.value} level`;
+    levelData = selectedMediumLevel(roadSymbolGenerated);
+    cardRender(levelData);
+  } else if (chooseLevelSelect.value === "hard") {
+    gameUserChosenLevelText.textContent = `You chose ${chooseLevelSelect.value} level`;
+    levelData = selectedHardLevel(roadSymbolGenerated);
+    cardRender(levelData);
+  } else {
+    mainGameSection.classList.remove("d-block");
+    enterGameSection.classList.remove("d-none");
+  }
+
+  if (
+    chooseTimeSelect.value !== "1" &&
+    chooseTimeSelect.value !== "10" &&
+    chooseTimeSelect.value !== "15"
+  ) {
+    mainGameSection.classList.remove("d-block");
+    enterGameSection.classList.remove("d-none");
+  }
+
+  startMinutes = Number(chooseTimeSelect.value);
+  setTimer(startMinutes);
+});
+
+// ! Choose level by select and slice 0 to 31
+function selectedEasyLevel(roadSymbol) {
+  const gameEasyLevel = roadSymbol.slice(0, 5);
+  sortedData = gameEasyLevel.map((item) => item.id);
+  return gameEasyLevel;
+}
+
+// ! Choose level by select and slice 0 to 51
+function selectedMediumLevel(roadSymbol) {
+  const gameMediumLevel = roadSymbol.slice(0, 51);
+  sortedData = gameMediumLevel.map((item) => item.id);
+  return gameMediumLevel;
+}
+
+// ! Choose level by select and slice 0 to array's length
+function selectedHardLevel(roadSymbol) {
+  const gameHardLevel = roadSymbol;
+  sortedData = gameHardLevel.map((item) => item.id);
+  return gameHardLevel;
+}
+
 // ! Timer count down
 let globalTime = 0;
 function setTimer(startTime) {
@@ -63,73 +120,9 @@ function updateCountDown() {
     return;
   }
 
-  //   gameOverModalBtn.addEventListener("click", () => {
-  //     gameOverModal.classList.add("d-none");
-  //     enterGameSection.classList.remove("d-none");
-  //     mainGameSection.classList.remove("d-block");
-  //   });
-
   seconds = seconds < 10 ? "0" + seconds : seconds;
   gameTimeText.textContent = `${minutes}:${seconds}`;
   globalTime--;
-}
-
-// ! Form before enter game
-enterGameBtn.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  enterGameSection.classList.add("d-none");
-  mainGameSection.classList.add("d-block");
-
-  if (chooseLevelSelect.value === "easy") {
-    gameUserChosenLevelText.textContent = `You chose ${chooseLevelSelect.value} level`;
-    levelData = selectedEasyLevel(roadSymbolGenerated);
-    cardRender(levelData);
-  } else if (chooseLevelSelect.value === "medium") {
-    gameUserChosenLevelText.textContent = `You chose ${chooseLevelSelect.value} level`;
-    levelData = selectedMediumLevel(roadSymbolGenerated);
-    cardRender(levelData);
-  } else if (chooseLevelSelect.value === "hard") {
-    gameUserChosenLevelText.textContent = `You chose ${chooseLevelSelect.value} level`;
-    levelData = selectedHardLevel(roadSymbolGenerated);
-    cardRender(levelData);
-  } else {
-    mainGameSection.classList.remove("d-block");
-    enterGameSection.classList.remove("d-none");
-  }
-
-  if (
-    chooseTimeSelect.value !== "5" &&
-    chooseTimeSelect.value !== "10" &&
-    chooseTimeSelect.value !== "15"
-  ) {
-    mainGameSection.classList.remove("d-block");
-    enterGameSection.classList.remove("d-none");
-  }
-
-  startMinutes = Number(chooseTimeSelect.value);
-  setTimer(startMinutes);
-});
-
-// ! Choose level by select and slice 0 to 31
-function selectedEasyLevel(roadSymbol) {
-  let gameEasyLevel = roadSymbol.slice(0, 31);
-  sortedData = gameEasyLevel.map((item) => item.id);
-  console.log(gameEasyLevel);
-  return gameEasyLevel;
-}
-// ! Choose level by select and slice 0 to 51
-function selectedMediumLevel(roadSymbol) {
-  let gameMediumLevel = roadSymbol.slice(0, 51);
-  sortedData = gameMediumLevel.map((item) => item.id);
-  console.log(gameMediumLevel);
-  return gameMediumLevel;
-}
-// ! Choose level by select and slice 0 to array's length
-function selectedHardLevel(roadSymbol) {
-  let gameHardLevel = roadSymbol;
-  sortedData = gameHardLevel.map((item) => item.id);
-  console.log(gameHardLevel);
-  return gameHardLevel;
 }
 
 // ! Main Array roadSymbol is Generated
@@ -140,22 +133,20 @@ roadSymbolGenerated = roadSymbol
 
 // ! Game's question randomizer
 function gameQuestionRandom(generatedQuestion) {
-  console.log(sortedData);
-  let randomIndex = Math.floor(Math.random() * sortedData.length);
+  const randomIndex = Math.floor(Math.random() * sortedData.length);
   let selectedId = sortedData[randomIndex];
 
   let selectedObject = generatedQuestion.find((item) => item.id == selectedId);
 
-  gameQuestionText.dataset.qestionId = selectedObject.id;
+  gameQuestionText.dataset.questionId = selectedObject.id;
   gameQuestionText.textContent = selectedObject.symbol_title;
-  console.log("Selected question", selectedObject);
 }
 
 // ! Main creating element and appenchild
 function cardRender(cardRules) {
   gameCardList.innerHTML = "";
 
-  let cardRulesFragment = document.createDocumentFragment();
+  const cardRulesFragment = document.createDocumentFragment();
 
   for (let gameCardRules of cardRules) {
     let cloneCardRulesTemplate = cardRulesTemplate.cloneNode(true);
@@ -171,8 +162,10 @@ function cardRender(cardRules) {
   // ! Winner modal
   if (sortedData.length == 0) {
     gameWinModal.classList.add("d-flex");
+    gameOverModal.classList.remove("d-flex");
     // gameWinModalErrorText.textContent = `You made ${gameErrorCounter} mistakes`;
     gameWinModalScoreText.textContent = `Your score is ${gameScoreCount}`;
+    setTimer(0);
   }
   gameQuestionRandom(cardRules);
   gameCardList.appendChild(cardRulesFragment);
@@ -181,15 +174,14 @@ function cardRender(cardRules) {
 // ! Event delegation
 gameCardList.addEventListener("click", (evt) => {
   if (evt.target.matches(".game-card-item")) {
-    let cardId = Number(evt.target.dataset.cardId);
-    console.log(cardId);
+    const cardId = Number(evt.target.dataset.cardId);
     let findedItem = evt.target;
     findedItem.style.backgroundColor = "red";
     setTimeout(() => {
       findedItem.style.backgroundColor = "transparent";
     }, 5000);
     // ! Check, If correct answer or not
-    if (cardId == gameQuestionText.dataset.qestionId) {
+    if (cardId == gameQuestionText.dataset.questionId) {
       let megaSortedData = sortedData.filter((item) => item !== cardId);
       sortedData = [...megaSortedData];
       gameScoreCount += 2;
@@ -212,4 +204,12 @@ gameCardList.addEventListener("click", (evt) => {
       }
     }
   }
+});
+
+aboutGameBtn.addEventListener("click", () => {
+  aboutGameBox.classList.add("d-block");
+});
+
+aboutCloseBtn.addEventListener("click", () => {
+  aboutGameBox.classList.remove("d-block");
 });
